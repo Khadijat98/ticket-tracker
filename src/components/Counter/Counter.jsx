0632a118c252d4931c70d487475fd3e542c7f633
Khadijat Oyeleye <kaydee98@hotmail.co.uk>
2021-12-02 14:04:57 +0000

    added state for counter

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Counter.scss";
 
 const Counter = () => {
+  const [counter, setCounter] = useState(0);
+
+  const handleIncrement = () => {
+    setCounter(counter + 1);
+  }
+
+  const handleDecrement = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  }
+
     return <div className="counter">
       <p className="counter__header">Counter</p>
       <div className="counter__button-container">
-        <button className="counter__button">+</button>
-        <button className="counter__button">-</button>
-      </div>  
+        <button onClick={handleIncrement} className="counter__button">+</button>
+        <p className="counter__button--text">{counter}</p>
+        <button onClick={handleDecrement} className="counter__button">-</button>
+      </div> 
     </div>
   
 }
